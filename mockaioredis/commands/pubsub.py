87189c1dea39004710bbd8f7696e74d29f4b02f5
Adam <l12lpo1@gmail.com>
2020-02-19 00:12:40 +0100

    Fix problem with async

--- mockaioredis/commands/pubsub.py
+++ mockaioredis/commands/pubsub.py
@@ -5,5 +5,5 @@ class PubSubCommandsMixin(PubSubCommandsMixin_):
     async def publish(self, channel, message):
         self._redis.publish(channel, message)
 
-    def subscribe(self, channel, *channels):
+    async def subscribe(self, channel, *channels):
         return self._redis.subscribe(channel, *channels)
